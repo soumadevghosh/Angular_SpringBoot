acbc3d01490a29e1b1bb6a2f43e1cf2f02696530
Soumadev <soumadevghosh@gmail.com>
2020-07-25 12:54:50 +0530

    v5

--- angular-with-springbootRest/src/main/java/com/angularwithspringboot/AngularWithSpringbootRestApplication.java
+++ angular-with-springbootRest/src/main/java/com/angularwithspringboot/AngularWithSpringbootRestApplication.java
@@ -8,5 +8,6 @@ public class AngularWithSpringbootRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AngularWithSpringbootRestApplication.class, args);
+		
 	}
 }
